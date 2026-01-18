e9de6e0585af57ac91a15d1b0c3ee31d327df93c
Stefan <99999147+StefanKordos@users.noreply.github.com>
2026-01-18 21:45:47 +0100

    PA6 configured for GPO

--- Core/Src/i2c.c
+++ Core/Src/i2c.c
@@ -25,9 +25,6 @@
 /* USER CODE END 0 */
 
 I2C_HandleTypeDef hi2c1;
-DMA_HandleTypeDef hdma_i2c1_tx;
-
-
 
 /* I2C1 init function */
 void MX_I2C1_Init(void)
@@ -90,32 +87,13 @@ void HAL_I2C_MspInit(I2C_HandleTypeDef* i2cHandle)
     */
     GPIO_InitStruct.Pin = GPIO_PIN_6|GPIO_PIN_7;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
     GPIO_InitStruct.Alternate = GPIO_AF4_I2C1;
     HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
     /* I2C1 clock enable */
     __HAL_RCC_I2C1_CLK_ENABLE();
-
-    /* I2C1 DMA Init */
-        /* I2C1_TX Init */
-        hdma_i2c1_tx.Instance = DMA1_Channel4;
-        hdma_i2c1_tx.Init.Direction = DMA_MEMORY_TO_PERIPH;
-        hdma_i2c1_tx.Init.PeriphInc = DMA_PINC_DISABLE;
-        hdma_i2c1_tx.Init.MemInc = DMA_MINC_ENABLE;
-        hdma_i2c1_tx.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
-        hdma_i2c1_tx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
-        hdma_i2c1_tx.Init.Mode = DMA_CIRCULAR;
-        hdma_i2c1_tx.Init.Priority = DMA_PRIORITY_LOW;
-        if (HAL_DMA_Init(&hdma_i2c1_tx) != HAL_OK)
-        {
-          Error_Handler();
-        }
-
-        __HAL_DMA_REMAP_CHANNEL_ENABLE(HAL_REMAPDMA_I2C1_TX_DMA1_CH4);
-
-        __HAL_LINKDMA(i2cHandle,hdmatx,hdma_i2c1_tx);
 
     /* I2C1 interrupt Init */
     HAL_NVIC_SetPriority(I2C1_EV_IRQn, 0, 0);
