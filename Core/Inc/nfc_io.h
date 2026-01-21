b68042f2f4f576f99eee4135f8b00907a619c317
Stefan <99999147+StefanKordos@users.noreply.github.com>
2026-01-21 20:06:08 +0100

    added nfc_io.h & .c

---
+++ Core/Inc/nfc_io.h
@@ -0,0 +1,20 @@
+/*
+ * nfc_io.h
+ *
+ *  Created on: Jan 21, 2026
+ *      Author: stefan
+ */
+
+#ifndef INC_NFC_IO_H_
+#define INC_NFC_IO_H_
+
+#include "m24sr.h"
+#include "stm32f3xx_ll_i2c.h"
+#include "stm32f3xx_ll_gpio.h"
+
+
+
+
+
+
+#endif /* INC_NFC_IO_H_ */
---
+++ Core/Src/nfc_io.c
@@ -0,0 +1,8 @@
+/*
+ * nfc_io.c
+ *
+ *  Created on: Jan 21, 2026
+ *      Author: stefan
+ */
+
+
