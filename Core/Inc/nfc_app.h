2e91c1f633c3f247490c4178cfafe8fdb116adc6
xjanicekr <126511887+xjanicekr@users.noreply.github.com>
2026-01-22 12:45:34 +0100

    Add NFC application interface for request processing

--- Core/Inc/nfc_app.h
+++ Core/Inc/nfc_app.h
@@ -8,6 +8,24 @@
 #ifndef INC_NFC_APP_H_
 #define INC_NFC_APP_H_
 
+/*Types of NFC request*/
+typedef enum {
+    NFC_REQ_INVALID = 0,
+    NFC_REQ_GET_JOKE,
+    NFC_REQ_ADD_JOKE
+} NFC_Request_t;
 
+/**
+ * @brief Analyze received message
+ * @param rxText Text received from NFC tag
+ * @return type of NFC request
+ */
+NFC_Request_t NFC_ParseRequest(char *rxText);
+
+/**
+ * @brief Process NFC request and write response to NFC tag
+ * @param rxText Text received from NFC tag
+ */
+void NFC_ProcessRequest(char *rxText);
 
 #endif /* INC_NFC_APP_H_ */
