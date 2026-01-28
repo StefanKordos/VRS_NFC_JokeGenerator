3ebc26deed4c13ae95b9dbcb3818b67e296ce691
xjanicekr <126511887+xjanicekr@users.noreply.github.com>
2026-01-28 12:50:38 +0100

    Implement NDEF Text (SR) encoder for M24SR

--- Core/Src/nfc_io.c
+++ Core/Src/nfc_io.c
@@ -86,3 +86,85 @@ uint16_t Read_NDEF_From_NFC(uint8_t *ndef_buffer, uint16_t buffer_size, uint16_t
 	return status;
 }
 
+uint8_t Convert_to_NDEF(char *text, uint8_t *ndef)
+{
+	if (text == NULL || ndef == NULL)
+		return 1;
+
+	/*Byte 0–1  → NLEN
+	Byte 2    → Flags
+	Byte 3    → Type Length
+	Byte 4    → Payload Length
+	Byte 5    → Type ('T')
+	Byte 6    → Status byte
+	Byte 7–8  → Language ("en")
+	Byte 9+   → Text
+	*/
+
+	uint16_t text_len = strlen(text);
+
+	//Status byte 1B, Language code 2B "en", text text_lenB
+	uint8_t payload_length = 1 + 2 + text_len; //NFCForum-TS-RTD_Text_1.0 p.4
+
+	//Length of NDEF record
+	/*
+	 *flags(MB ME CF SR IL TNF) 1B
+	 *TYPE LENGTH 1B
+	 *PAYLOAD_LENGTH (SR=1 -> 1 byte, NDEF.pdf 3.2.4) enough for joke
+	 *TYPE 1B
+	 *PAYLOAD payload_lengthB*/
+	uint16_t ndef_record_length = 4 + payload_length; //NFCForum-TS-NDEF_1.0 p.14
+
+	// NLEN (2B) – NDEF message length in big-endian (Type 4 Tag, M24SR64-Y p.23)
+	//MSB first NFCForum-TS-NDEF_1.0 p.13
+	//this is not part of NDEF record
+	ndef[0] = (ndef_record_length >> 8) & 0xFF; //MSB shifting to right
+	ndef[1] = (ndef_record_length ) & 0xFF; //LSB
+
+	//-------- NDEF RECORD NFCForum-TS-NDEF_1.0 s.3.2.4--------
+	/*
+	 * MB=1, ME=1, CF=0, IL=0
+	 * SR=1 -> Short Record
+	 * TNF=001 -> Well Known Type*/
+	ndef[2] = 0xD1; //11010001
+
+	//-------- Text record SR(Short Record) NFCForum-TS-NDEF_1.0 s.3.2 --------
+	/*
+	 * TYPE_LENGTH = 1
+	 * (single character 'T')
+	 */
+	ndef[3] = 0x01;
+
+	/*
+	 * PAYLOAD_LENGTH (SR=1 → 1 byte)
+	 */
+	ndef[4] = payload_length;
+
+	/*
+	 * TNF=0x01 → RTD type namespace
+	 */
+	ndef[5] = 0x54;  // ASCII 'T'
+
+	//-------- PAYLOAD NFCForum-TS-NDEF_1.0 s.3.2--------
+	/*
+	 * Payload = Status Byte + Language Code + Text
+	 * Bit number (0 is LSB)
+	 * Status byte:
+	 * Bit 7 = 0 → UTF-8 encoding
+	 * Bits 5..0 = language length (2 bytes → "en")
+	 *
+	 * Defined in NFC RTD Text specification
+	 */
+
+	//bit7  bit6  bit5..0
+	// 0     0      000010 2B for "en"
+	ndef[6] = 0x02;
+
+	//Language Code
+	ndef[7] = 'e';
+	ndef[8] = 'n';
+
+	memcpy(&ndef[9], text, text_len); //copy length of bytes without \0
+	return 0;
+}
+
